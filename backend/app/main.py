38f247532053ac53f6fe68ca7fd1509674a0b89d
Andrew Kim <andkim123@gmail.com>
2023-12-25 19:29:57 -0800

    model prediction and actual prediction functions

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,56 +1,63 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import os
-
-# Disable GPU, comment if you want to use GPU.
-tf.config.set_visible_devices([], 'GPU')
+from typing import List
 
 app = FastAPI()
 
-# Configure CORS
+# CORS configuration for cross-origin requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Change this to use your domain
+    allow_origins=["*"], # Configure the allowed origins as needed
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Define a Pydantic model for the grid data
+# Pydantic model for grid data
 class Grid(BaseModel):
-    data: list
+    data: List[List[int]]
 
-# Load TensorFlow model
-def dice_coefficient(y_true, y_pred, smooth=1e-6):
+# Custom metric function for TensorFlow model
+def dice_coefficient(y_true: tf.Tensor, y_pred: tf.Tensor, smooth: float = 1e-6) -> tf.Tensor:
     y_true_f = tf.reshape(y_true, [-1])
     y_pred_f = tf.reshape(y_pred, [-1])
     intersection = tf.reduce_sum(y_true_f * y_pred_f)
     union = tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f)
     return (2. * intersection + smooth) / (union + smooth)
 
+# Path setup for the TensorFlow model
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(BASE_DIR, 'models', 'gol_10000_2_100_100_model.h5')
-model = keras.models.load_model(model_path, custom_objects={'dice_coefficient': dice_coefficient})
 
-@app.get("/initialize/")
-async def initialize_grid():
+# Model loading with error handling
+try:
+    model = keras.models.load_model(model_path, custom_objects={'dice_coefficient': dice_coefficient})
+except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
+
+# Endpoints
+@app.get("/initialize/", response_model=Grid)
+async def initialize_grid() -> Grid:
+    # Endpoint to initialize a 100x100 grid
     grid = np.zeros((100, 100), dtype=int).tolist()
     return {"grid": grid}
 
-"""
-@app.post("/model_predict/")
-async def model_predict(grid: Grid):
+@app.post("/model_predict/", response_model=Grid)
+async def model_predict(grid: Grid) -> Grid:
+    # Endpoint to predict the next state of the grid
     prediction_grid = np.array(grid.data)
     predicted_next_state = model.predict(np.expand_dims(np.expand_dims(prediction_grid, axis=0), axis=-1))[0]
     predicted_next_state = (predicted_next_state > 0.5).astype(int)
     predicted_next_state = predicted_next_state.reshape(100, 100)
     return {"prediction": predicted_next_state.tolist()}
 
-def simulate_game_of_life(grid):
+def simulate_game_of_life(grid: List[List[int]]) -> List[List[int]]:
+    # Function to simulate the Game of Life rules
     extended_grid = np.pad(grid, pad_width=1, mode='wrap')
     next_state = np.zeros_like(grid)
 
@@ -64,9 +71,9 @@ def simulate_game_of_life(grid):
 
     return next_state
 
-@app.post("/actual_predict/")
-async def actual_predict(grid: Grid):
+@app.post("/actual_predict/", response_model=Grid)
+async def actual_predict(grid: Grid) -> Grid:
+    # Endpoint to predict using the actual game rules
     current_state = np.array(grid.data)
     next_state = simulate_game_of_life(current_state)
     return {"prediction": next_state.tolist()}
-    """
--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,14 +1,57 @@
 from fastapi.testclient import TestClient
-from ..app.main import app  # Replace with the name of your FastAPI application file
+from ..app.main import app
 
+# Initialize TestClient
 client = TestClient(app)
 
 def test_initialize_grid():
+    """
+    Test the '/initialize/' endpoint to ensure it returns
+    a 100x100 grid initialized to zeros.
+    """
     response = client.get("/initialize/")
     assert response.status_code == 200
+    grid = response.json()['grid']
     assert 'grid' in response.json()
-    assert len(response.json()['grid']) == 100
-    assert len(response.json()['grid'][0]) == 100
-    assert response.json()['grid'][0][0] == 0  # Assumes the grid is initialized to zeros
+    assert len(grid) == 100 and len(grid[0]) == 100
+    assert all(all(cell == 0 for cell in row) for row in grid)
 
-# Add more tests for other endpoints
+def test_model_predict():
+    """
+    Test the '/model_predict/' endpoint with a 100x100 empty grid.
+    The endpoint's response structure and status code are validated.
+    """
+    test_grid = {'data': [[0 for _ in range(100)] for _ in range(100)]}
+    response = client.post("/model_predict/", json=test_grid)
+    assert response.status_code == 200
+    assert 'prediction' in response.json()
+    prediction = response.json()['prediction']
+    assert len(prediction) == 100 and len(prediction[0]) == 100
+
+def test_actual_predict_glider():
+    """
+    Test the '/actual_predict/' endpoint with the first iteration
+    of the glider pattern. Validates that the response matches the
+    expected second iteration of the glider pattern.
+    """
+    # Define the initial glider pattern
+    glider_grid = [[0 for _ in range(100)] for _ in range(100)]
+    glider_grid[1][2] = 1
+    glider_grid[2][3] = 1
+    glider_grid[3][1] = glider_grid[3][2] = glider_grid[3][3] = 1
+
+    # Test the endpoint
+    response = client.post("/actual_predict/", json={"data": glider_grid})
+    assert response.status_code == 200
+    result_grid = response.json()['prediction']
+
+    # Define the expected pattern after one iteration
+    expected_grid = [[0 for _ in range(100)] for _ in range(100)]
+    expected_grid[1][3] = 1
+    expected_grid[2][1] = expected_grid[2][3] = 1
+    expected_grid[3][2] = expected_grid[3][3] = 1
+
+    # Check that the result matches the expected pattern
+    for i in range(100):
+        for j in range(100):
+            assert result_grid[i][j] == expected_grid[i][j]
