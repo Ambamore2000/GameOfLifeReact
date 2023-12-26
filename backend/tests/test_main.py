7d6dd4cf245b05a0ec0114c78ddc10e021c0e02b
Andrew Kim <andkim123@gmail.com>
2023-12-25 20:55:35 -0800

    test case with 5x5 grids

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -3,10 +3,18 @@ from ..app.main import app
 
 client = TestClient(app)
 
+def print_first_5x5(grid):
+    for row in grid[:5]:
+        print(row[:5])
+
 def test_initialize_grid():
     response = client.get("/initialize/")
     assert response.status_code == 200
     grid = response.json()['grid']
+
+    print("Initialize Grid:")
+    print_first_5x5(grid)
+
     assert 'grid' in response.json()
     assert len(grid) == 100 and len(grid[0]) == 100
     assert all(all(cell == 0 for cell in row) for row in grid)
@@ -18,6 +26,10 @@ def test_model_predict():
         print("Response content:", response.content)
     assert response.status_code == 200
     prediction = response.json()['prediction']
+
+    print("Model Predict Grid:")
+    print_first_5x5(prediction)
+
     assert 'prediction' in response.json()
     assert len(prediction) == 100 and len(prediction[0]) == 100
 
@@ -27,18 +39,25 @@ def test_actual_predict_glider():
     glider_grid_data[2][3] = 1
     glider_grid_data[3][1] = glider_grid_data[3][2] = glider_grid_data[3][3] = 1
 
+    print("Simulation Input Grid:")
+    print_first_5x5(glider_grid_data)
+
     response = client.post("/actual_predict/", json={'data': glider_grid_data})
     if response.status_code != 200:
         print("Response content:", response.content)
     assert response.status_code == 200
     result_grid = response.json()['prediction']
 
-    # Expected grid format after the glider moves
+    print("Simulation Output Grid:")
+    print_first_5x5(result_grid)
+
     expected_grid = [[0 for _ in range(100)] for _ in range(100)]
     expected_grid[1][3] = 1
     expected_grid[2][1] = expected_grid[2][3] = 1
     expected_grid[3][2] = expected_grid[3][3] = 1
 
+    print("Simulation Expected Grid:")
+    print_first_5x5(expected_grid)
     for i in range(100):
         for j in range(100):
             assert result_grid[i][j] == expected_grid[i][j]
